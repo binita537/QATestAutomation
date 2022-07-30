04daf797d40c5edf77f3620d4634bfc6e8dcba21
Binita <binitarathour02@gmail.com>
2022-07-30 17:04:54 +0530

    added driver factory implementation

--- src/main/java/com/orangehrm/browsers/BrowserType.java
+++ src/main/java/com/orangehrm/browsers/BrowserType.java
@@ -1,5 +1,8 @@
 package com.orangehrm.browsers;
 
+
 public enum BrowserType {
+	
+	CHROME,FIREFOX,IE;
 
 }
--- src/main/java/com/orangehrm/browsers/DriverFactory.java
+++ src/main/java/com/orangehrm/browsers/DriverFactory.java
@@ -1,7 +1,37 @@
 package com.orangehrm.browsers;
 
-public class DriverFactory {
-	   
-	
+import org.apache.commons.collections4.functors.SwitchClosure;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DriverFactory {
+	
+	
+	
+	public static WebDriverInstance getDriverInstance(BrowserType browserType) throws Exception
+	{
+		WebDriverInstance webDriverInstance;
+		
+			
+
+     if(browserType==BrowserType.CHROME)
+     {
+    	WebDriverManager.chromedriver().setup();
+    	webDriverInstance=new ChromeDriverInstance();
+    	 
+     }
+     else if (browserType==BrowserType.FIREFOX)
+     {
+    	 WebDriverManager.firefoxdriver().setup();
+     	webDriverInstance=new FirefoxDriverInstance();  	 
+    	 
+     }
+     else {
+    	 
+    	 throw new Exception("PLease provide correct name of the driver:"+browserType+"this driver is not valid");
+     }
+	
+     return webDriverInstance;
+
+}
 }
